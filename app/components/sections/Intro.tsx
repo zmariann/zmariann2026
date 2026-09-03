9153aaa6d94a3e66a48b7478db3649a7c49dfe71
mariann <106428909+zmariann@users.noreply.github.com>
2026-09-03 16:26:20 +0200

    fix: remove non-breaking space from intro text

--- app/components/sections/Intro.tsx
+++ app/components/sections/Intro.tsx
@@ -4,7 +4,7 @@ import { Text } from "../ui/typography/Text";
 export function Intro() {
     return (
         <section className="flex flex-col items-center justify-center gap-5">
-            <Heading level={1} color="carrot">However you found your way here, you are probably looking for a creative&nbsp;partner.</Heading>
+            <Heading level={1} color="carrot">However you found your way here, you are probably looking for a creative partner.</Heading>
             <Text>Someone with experience working on real-world&nbsp;projects,
 </Text>
             <Text>with an understanding of the process from start&nbsp;to&nbsp;finish.</Text>
